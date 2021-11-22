6cca80407969516503df606d664e3a58251dd84d
chendren <chad.hendren@gmail.com>
2021-11-21 21:32:32 -0600

    Still code issues, but submitted

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,22 +1,25 @@
 # Chad Hendren
 # Bellevue University
 # CSD-310 November 21, 2021
-# Module 5 pytech_insert.py assignment
+# Module 5 pytech_delete.py assignment
 
 import pymongo
 from pymongo import MongoClient
 from pymongo.collation import CollationAlternate
 url = "mongodb+srv://admin:admin@cluster0.fsuv8.mongodb.net/pytech?retryWrites=true&w=majority"
 client = MongoClient(url)
-db = client.pytech
-mycol = db["students"]
+db = client.pytech.students
 
-docs = db.mycol.find({})
-print(docs)
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+doc = db.find_one({"student_id": "1007"})
+stud_id = str(doc["student_id"])
+print("Student ID:  "+(stud_id))
+print("First Name:  "+doc["first_name"])
+print("Last Name:  "+doc["last_name"]+'\n')
 
-myquery = { "student_id": "1007" }
-
-doc = mycol.find(myquery)
-print("Student ID: "+(doc["student_id"])
-print("First Name: "+(doc["first_name"])
-print("Last Name: "+(doc["last_name"])
+docs = db.find({})
+for document in docs:
+    stud_id = str(docs["student_id"])
+    print("Student ID:  "+(stud_id))
+    print("First Name:  "+docs["first_name"])
+    print("Last Name:  "+docs["last_name"]+'\n')
