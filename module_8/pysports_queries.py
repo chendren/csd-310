a2756fc1203f979f8dd01fa601fc46b011149823
chendren <chad.hendren@gmail.com>
2021-11-28 18:17:07 -0600

    Updates query file

--- module_8/pysports_queries.py
+++ module_8/pysports_queries.py
@@ -1,35 +1,25 @@
 import mysql.connector
-from mysql.connector import errorcode
+from mysql.connector import Error
 
-config = {
-    "user": "root",
-    "password": "Ostrich%2020",
-    "host": "127.0.0.1",
-    "database": "pysports",
-    "raise_on_warnings": True
-}
 try:
-    db = mysql.connector.connect(**config)
+    connection = mysql.connector.connect(host='localhost',
+                                         database='pysports',
+                                         user='root',
+                                         password='Ostrich%2020')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
 
-    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
-
-    input("\n\n Press any key to continue. . . ")
-
-except mysql.connector.Error as err:
-        
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("*  The supplied username or password are invalid  *")
-
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("*  The specified database does not exist  *")
-
-    else:
-                print(err)
-
+except Error as e:
+    print("Error while connecting to MySQL", e)
 finally:
-        db.close()
+        print("MySQL connection is open")
 
-cursor = db.cursor()
+cursor = connection.cursor()
 
 cursor.execute("SELECT team_id, team_name, mascot FROM team")
 
@@ -37,19 +27,21 @@ teams = cursor.fetchall()
 
 print("-- DISPLAYING TEAM RECORDS --")
 for team in teams:
-    print("Team ID: {}".format(team[1]))
-    print("Team Name: {}".format(team[2]))
-    print("Mascot: {}".format(team[3]))
+    print("Team ID: {}".format(team[0]))
+    print("Team Name: {}".format(team[1]))
+    print("Mascot: {}".format(team[2]))
+    print("")
 
 cursor.execute("SELECT player_id, first_name, last_name, team_id FROM players")
 
-players = cursor.fetchall()
+players2 = cursor.fetchall()
 
 print("-- DISPLAYING PLAYER RECORDS --")
-for team in teams:
-    print("Player ID: {}".format(players[1]))
-    print("First Name: {}".format(players[2]))
-    print("Last Name: {}".format(players[3]))
-    print("Team ID: {}".format(players[4]))
+for players in players2:
+    print("Player ID: {}".format(players[0]))
+    print("First Name: {}".format(players[1]))
+    print("Last Name: {}".format(players[2]))
+    print("Team ID: {}".format(players[3]))
+    print("")
 
 input("\n\n Press any key to continue. . . ")
